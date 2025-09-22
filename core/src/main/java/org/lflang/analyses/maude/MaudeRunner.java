15cf53af08ab0b33a5ee9ac5d41278b73a6f08df
Mikheil Rukhaia <mrukhaia@gmail.com>
2025-09-22 12:00:52 +0400

    Modified Maude runner to take look for path to Maude 3.3.1 in the MAUDE_BASE environment variable

--- core/src/main/java/org/lflang/analyses/maude/MaudeRunner.java
+++ core/src/main/java/org/lflang/analyses/maude/MaudeRunner.java
@@ -24,39 +24,51 @@ public class MaudeRunner {
             new GeneratorCommandFactory(
                 generator.context.getErrorReporter(), generator.context.getFileConfig());
     }
+/*
+* Mikheil: we need to consider that our implementation needs Maude 3.3.1.
+*         In my machine "maude" command runs "default" installation of Maude 3.2
+*         I think it is best to have MAUDE_BASE environment variable pointing to Maude 3.3.1 folder
+*         I modified the implementation accordingly
+ */
+    public Path getEnvironmentVariable(String envVarName, String fileNameToExists) {
+        String envVar = System.getenv(envVarName);
+        if (envVar == null || envVar.isBlank()) {
+            throw new IllegalStateException("Environment variable "+envVarName+" is not set.");
+        }
+        Path envPath = Paths.get(envVar);
+
+        if (!Files.exists(envPath)) {
+            throw new IllegalStateException(envVarName+" pointing to path does not exist: " + envVar);
+        }
+        if (!Files.isDirectory(envPath)) {
+            throw new IllegalStateException(envVarName+" not pointing to a directory: " + envVar);
+        }
+        if (!Files.isReadable(envPath)) {
+            throw new IllegalStateException(envVarName+" pointing to path is not readable: " + envVar);
+        }
+        Path fullPath = envPath.resolve(fileNameToExists);
+        if (!Files.exists(fullPath)) {
+            throw new IllegalStateException("Required file not found: " + fullPath
+                + ". Please make sure that LF_MAUDE_BASE is pointing to the LF Maude implementation "
+                + "and MAUDE_BASE is ponting to Maude 3.3.1.");
+        }
+        if (!Files.isReadable(fullPath)) {
+            throw new IllegalStateException("Required file is not readable: " + fullPath);
+        }
+
+        return fullPath;
+    }
 
     public void run() {
-        String _lf_maudeBase = System.getenv("LF_MAUDE_BASE");
-        if (_lf_maudeBase == null || _lf_maudeBase.isBlank()) {
-            throw new IllegalStateException("Environment variable LF_MAUDE_BASE is not set.");
-        }
-
-        Path maudeBase = Paths.get(_lf_maudeBase);
-
-        if (!Files.exists(maudeBase)) {
-            throw new IllegalStateException("LF_MAUDE_BASE does not exist: " + maudeBase);
-        }
-        if (!Files.isDirectory(maudeBase)) {
-            throw new IllegalStateException("LF_MAUDE_BASE is not a directory: " + maudeBase);
-        }
-        if (!Files.isReadable(maudeBase)) {
-            throw new IllegalStateException("LF_MAUDE_BASE is not readable: " + maudeBase);
-        }
-        Path lfMain = maudeBase.resolve("lf-main-concrete.maude");
-        if (!Files.exists(lfMain)) {
-            throw new IllegalStateException("Required file not found: " + lfMain);
-        }
-        if (!Files.isReadable(lfMain)) {
-            throw new IllegalStateException("Required file is not readable: " + lfMain);
-        }
-
+        Path lf_maudeBase =getEnvironmentVariable("LF_MAUDE_BASE","lf-main-concrete.maude");
+        Path maudeBase =getEnvironmentVariable("MAUDE_BASE","maude");
 
         for (Path path : generator.generatedFiles) {
             LFCommand command =
                 commandFactory.createCommand(
-                    "maude",
+                    maudeBase.toString(),
                     List.of(
-                        lfMain.toString(),
+                        lf_maudeBase.toString(),
                         path.toString(),
                         "-xml-log="+path.toString()+".xml"
                     ),
