448689264c34faef61463166318e6d3212b2b5b0
mrd <reja.mario@gmail.com>
2026-07-27 00:17:55 +0300

    Maude: allow one-short timers
    Initial implementation disallowed a period of 0 for timers. This is
    corrected now.

--- core/src/main/java/org/lflang/analyses/maude/MaudeTimerInstance.java
+++ core/src/main/java/org/lflang/analyses/maude/MaudeTimerInstance.java
@@ -4,54 +4,54 @@ import org.lflang.generator.TimerInstance;
 
 public class MaudeTimerInstance {
 
-    private final TimerInstance lfTimer;
-    private String name;
-    private final long offset;
-    private final long period;
+  private final TimerInstance lfTimer;
+  private String name;
+  private final long offset;
+  private final long period;
 
-    MaudeReactorInstance parent;
+  MaudeReactorInstance parent;
 
-    public MaudeTimerInstance(TimerInstance lfTimer, MaudeReactorInstance parent) {
-        this.lfTimer = lfTimer;
-        this.parent = parent;
+  public MaudeTimerInstance(TimerInstance lfTimer, MaudeReactorInstance parent) {
+    this.lfTimer = lfTimer;
+    this.parent = parent;
 
-        this.name = MaudeIdentifiers.timer(parent, lfTimer);
+    this.name = MaudeIdentifiers.timer(parent, lfTimer);
 
-        // TODO: Add explicit time units to Maude
-        // Our Maude implementation does not use time units.
-        // Using nanoseconds (the most granular LF time unit) everywhere is correct and trivial,
-        // but will make reading Maude's output contain large numbers whenever seconds/milliseconds are used
-        this.offset = lfTimer.getOffset().toNanoSeconds();
-        this.period = lfTimer.getPeriod().toNanoSeconds();
+    // TODO: Add explicit time units to Maude
+    // Our Maude implementation does not use time units.
+    // Using nanoseconds (the most granular LF time unit) everywhere is correct and trivial,
+    // but will make reading Maude's output contain large numbers whenever seconds/milliseconds are
+    // used.
+    this.offset = lfTimer.getOffset().toNanoSeconds();
+    this.period = lfTimer.getPeriod().toNanoSeconds();
 
-
-
-        if (this.offset < 0 || this.period <= 0)
-            throw new RuntimeException("Offset and Period must be non-negative. Period cannot be 0.");
+    if (this.offset < 0 || this.period < 0) {
+      throw new RuntimeException("Timer offset and period must be non-negative.");
     }
+  }
 
-    public MaudeReactorInstance getParent() {
-        return parent;
-    }
+  public MaudeReactorInstance getParent() {
+    return parent;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public long getOffset() {
-        return offset;
-    }
+  public long getOffset() {
+    return offset;
+  }
 
-    public long getPeriod() {
-        return period;
-    }
+  public long getPeriod() {
+    return period;
+  }
 
-    public TimerInstance getLfTimer() {
-        return lfTimer;
-    }
+  public TimerInstance getLfTimer() {
+    return lfTimer;
+  }
 
-    @Override
-    public String toString() {
-        return this.getName();
-    }
+  @Override
+  public String toString() {
+    return this.getName();
+  }
 }
